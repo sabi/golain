58d22b5f495d113c7c54f5712cb8cd1564bc656d
Sabi <49737728+sabi@users.noreply.github.com>
2023-03-15 20:43:34 -0700

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,37 @@
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 */
-package main
+package cmd
 
-import "golain/cmd"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
-func main() {
-	cmd.Execute()
+	"github.com/spf13/cobra"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+var defaultChannel string
+
+func init() {
+	// Set up the SQLite database
+	var err error
+	db, err = sql.Open("sqlite3", "./golain.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the channels table
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS channels (name TEXT PRIMARY KEY, webhook TEXT, is_default INTEGER)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get the default channel
+	row := db.QueryRow("SELECT name FROM channels WHERE is_default = 1")
+	row.Scan(&defaultChannel)
 }
