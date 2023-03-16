79a3f733ca9a0f35357a13ae87b641a53ed5a478
Sabi <49737728+sabi@users.noreply.github.com>
2023-03-15 20:43:55 -0700

    Update add.go

--- cmd/add.go
+++ cmd/add.go
@@ -6,57 +6,35 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"io/ioutil"
+
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
+var channelFlag string
+var webhookFlag string
+
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "Add a new Discord Webhook",
-	Long: `Add a new Discord Webhook
-	golain add --channel General-Chat --webhook https://discord.com/api/123/123`,
+	Short: "Add a new channel and webhook",
 	Run: func(cmd *cobra.Command, args []string) {
-		line := []byte(Channel + " = " + Webhook + "\n") // put string into byte array
-		_, err := os.Stat("lain.conf") // Check for file
-		if err != nil { // if file does not exist
-			err := ioutil.WriteFile("lain.conf", line, 0600)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else { // if file did exist
-			f, err := os.OpenFile("lain.conf", os.O_APPEND|os.O_WRONLY, 0600)
-			if err != nil {
-				panic(err)
-			}
-		defer f.Close() // Close the file after the block of text is done
-		if Channel == "" {
-			fmt.Println("Channel not set") 
+		if channelFlag == "" || webhookFlag == "" {
+			fmt.Println("Both --channel and --webhook flags are required.")
 			return
 		}
-		if Webhook == "" {
-			fmt.Println("Webhook not set") 
+
+		_, err := db.Exec("INSERT INTO channels (name, webhook, is_default) VALUES (?, ?, ?)", channelFlag, webhookFlag, 0)
+		if err != nil {
+			fmt.Println("Failed to add channel:", err)
 			return
 		}
-		line := Channel + " = " + Webhook + "\n"
-		if _, err = f.WriteString(line); err != nil {
-			panic(err)
-		}
-	}
+
+		fmt.Println("Channel added:", channelFlag)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addCmd.Flags().StringVarP(&channelFlag, "channel", "c", "", "Channel name")
+	addCmd.Flags().StringVarP(&webhookFlag, "webhook", "w", "", "Webhook URL")
 }
