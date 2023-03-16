0beca7d969bd21acf74e4b3c209c098006bbcc59
Sabi <49737728+sabi@users.noreply.github.com>
2023-03-15 20:44:31 -0700

    Update ls.go

--- cmd/ls.go
+++ cmd/ls.go
@@ -6,42 +6,19 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
-	"reflect"
+
 	"github.com/spf13/cobra"
 )
 
-// lsCmd represents the ls command
 var lsCmd = &cobra.Command{
 	Use:   "ls",
-	Short: "List available Discord Channels",
-	Long: `golain ls`,
+	Short: "List all available channels",
 	Run: func(cmd *cobra.Command, args []string) {
-		lainHooks := make(map[string]string)
-		lines, err := readLines("lain.conf")
+		rows, err := db.Query("SELECT name FROM channels")
 		if err != nil {
+			fmt.Println("Failed to list channels:", err)
 			return
 		}
-		for _, line := range lines {
-			split := strings.Split(line, " = ")
-			lainHooks[split[0]] = split[1]
-	}
-		keys := reflect.ValueOf(lainHooks).MapKeys()
-		for _, discordChannel := range keys {
-			fmt.Println(discordChannel)}
-	},
-}
+		defer rows.Close()
 
-func init() {
-	rootCmd.AddCommand(lsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+		fmt.Println
